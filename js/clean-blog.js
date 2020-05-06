c46f0af65f0249d608a7fc9ddb018153c19dc6e2
deji <adedejiadedoyin@hotmail.com>
2020-05-06 19:43:41 +0100

    committed

--- js/clean-blog.js
+++ js/clean-blog.js
@@ -147,15 +147,34 @@ var slideUp = {
 ScrollReveal().reveal(".slideUp", slideUp);
 
 //---- STICKY ON SCROLL-------
-/*
-$(document).scroll(function (e) {
-  var scrollTop = $(document).scrollTop();
-  if (scrollTop > 0) {
-    console.log(scrollTop);
-    $(".navbar").addClass("fixed-top");
-  } else {
-    console.log(scrollTop);
-    $(".navbar").removeClass("fixed-top");
-  }
-});
-*/
+// Show the navbar when the page is scrolled up
+var MQL = 992;
+
+//primary navigation slide-in effect
+if ($(window).width() > MQL) {
+  var headerHeight = $("#mainNav").height();
+  $(window).on(
+    "scroll",
+    {
+      previousTop: 0,
+    },
+    function () {
+      var currentTop = $(window).scrollTop();
+      //check if user is scrolling up
+      if (currentTop < this.previousTop) {
+        //if scrolling up...
+        if (currentTop > 0 && $("#mainNav").hasClass("is-fixed")) {
+          $("#mainNav").addClass("is-visible");
+        } else {
+          $("#mainNav").removeClass("is-visible is-fixed");
+        }
+      } else if (currentTop > this.previousTop) {
+        //if scrolling down...
+        $("#mainNav").removeClass("is-visible");
+        if (currentTop > headerHeight && !$("#mainNav").hasClass("is-fixed"))
+          $("#mainNav").addClass("is-fixed");
+      }
+      this.previousTop = currentTop;
+    }
+  );
+}
